27fc874dae5ff76baa52b28291426115acd4c63b
Colin McNeill <colinpmcneill@gmail.com>
2014-11-10 19:15:28 -0500

    Create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
 
-def index
-  @posts = Post.all
-end
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :link, :body))
+    post.save
+    redirect_to posts_path
+  end
 
 end
