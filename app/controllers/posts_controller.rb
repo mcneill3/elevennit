5b3c5a369a45b12839deff01fe4eec722fca1e6f
Colin McNeill <colinpmcneill@gmail.com>
2014-11-10 20:06:36 -0500

    new page when created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,11 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(params.require(:post).permit(:title, :link, :body))
-    post.save
-    redirect_to posts_path
+    if post.save
+      redirect_to posts_path
+    else
+      redirect_to new_posts_path
+    end
   end
 
 end
