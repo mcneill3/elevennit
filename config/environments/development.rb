3d34ab73b8e5c9219dab9d5cd0e530b7c2bbfb2f
Colin McNeill <colinpmcneill@gmail.com>
2014-11-12 10:49:03 -0500

    raise exeptions for categories

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -21,6 +24,8 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+
+  config
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
