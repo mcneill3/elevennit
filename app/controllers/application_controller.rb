6f56ba25a90fcc782286eedc92cf3c103b239d95
Colin McNeill <colinpmcneill@gmail.com>
2014-11-12 12:55:21 -0500

    shorthand categories method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :categories
 
   def categories
-    @categories = Category.all if !defined? @categories
-    @categories
+    # @categories = Category.all if !defined? @categories
+    @categories ||= Category.all
   end
 end
