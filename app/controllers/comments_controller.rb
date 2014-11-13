8975013306bfaf22b448c201d0afa845b3c7a615
Colin McNeill <colinpmcneill@gmail.com>
2014-11-13 10:22:41 -0500

    extract new comment form into partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     post = Post.find params[:comment][:commentable_id]
     comment = Comment.build_from post, current_user.id, comment_params[:body]
+    comment.parent_id = params[:comment][:parent_id]
     if comment.save
       flash[:notice] = 'Your comment has been added.'
     else
